5beca7ac69539f0713db9208912bb3e1d1835c0d
jonathandavidblack <jonnybl915@gmail.com>
2016-07-05 12:18:14 -0400

    Initial Commit

---
+++ src/people_clojure/core.clj
@@ -0,0 +1,17 @@
+ (ns people-clojure.core
+  (:require [clojure.string :as str]) ;this is our import statement
+  (:gen-class))                         ;;comments are written after semicolons!!!
+
+(defn -main []
+  (let [people (slurp "people.csv")
+        people (str/split-lines people)
+        people (map (fn [line] (str/split line #","))
+                 people)
+        header (first people)
+        people (rest people)
+        people (map (fn [line] (zipmap header line))
+                 people)
+        people (filter (fn [line] (= "Brazil" (get line "country")))
+                 people)]
+    people))
+
